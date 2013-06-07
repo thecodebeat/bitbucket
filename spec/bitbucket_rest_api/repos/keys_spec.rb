2df942cb6dd2f53d56b875f6b1b4fad170d561c7
Clemens Helm <clemens.helm@gmail.com>
2013-06-07 13:15:01 +0200

    Fixed bug that dashes were stripped from the repository name.

--- spec/bitbucket_rest_api/repos/keys_spec.rb
+++ spec/bitbucket_rest_api/repos/keys_spec.rb
@@ -2,11 +2,20 @@
 require 'bitbucket_rest_api'
 
 describe BitBucket::Repos::Keys do
-  it "creates a hook for a repository with special characters" do
-    repository_name = "Funnü ChAraxt%"
-    repository_url_name = "funn-charaxt"
-    keys = BitBucket::Repos::Keys.new
-    keys.should_receive(:post_request).with "/repositories/user/#{repository_url_name}/deploy-keys/", anything
-    keys.create "user", repository_name, label: '', key: ''
+  context "creating a key" do
+    let(:keys) { BitBucket::Repos::Keys.new }
+    
+    it "should work for a repository with special characters" do
+      repository_name = "Funnü ChAraxt%"
+      repository_url_name = "funn-charaxt"
+      keys.should_receive(:post_request).with "/repositories/user/#{repository_url_name}/deploy-keys/", anything
+      keys.create "user", repository_name, label: '', key: ''
+    end
+  
+    it "should preserve dashes in the repository name" do
+      repository_name = "my-repo"
+      keys.should_receive(:post_request).with "/repositories/user/#{repository_name}/deploy-keys/", anything
+      keys.create "user", repository_name, label: '', key: ''
+    end
   end
 end
