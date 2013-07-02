3b23099f4521fcf6d7c30f15bf18a290fe513b65
beanieboi <beanie@benle.de>
2013-07-02 14:33:24 +0200

    remove not needed include code

--- lib/bitbucket_rest_api/helpers/repository_helper.rb
+++ lib/bitbucket_rest_api/helpers/repository_helper.rb
@@ -2,19 +2,9 @@ module BitBucket
   module Helpers
     module RepositoryHelper
 
-      module ClassMethods
-      end
-
-      module InstanceMethods
-        def sanitize_repository_name(repository_name)
-          return nil if repository_name.nil?
-          repository_name.downcase.gsub(/[^a-z0-9\_\-\. ]/, '').gsub(' ', '-')
-        end
-      end
-
-      def self.included(receiver)
-        receiver.extend         ClassMethods
-        receiver.send :include, InstanceMethods
+      def sanitize_repository_name(repository_name)
+        return nil if repository_name.nil?
+        repository_name.downcase.gsub(/[^a-z0-9\_\-\. ]/, '').gsub(' ', '-')
       end
 
     end
