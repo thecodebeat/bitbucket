d199045bc4bb87e4e70ca650a345185285cda189
Mauro Morales <contact@mauromorales.com>
2019-04-17 15:39:36 +0200

    Also part or rollback for unused endpoints

--- lib/bitbucket_rest_api/repos.rb
+++ lib/bitbucket_rest_api/repos.rb
@@ -187,7 +187,7 @@ module BitBucket
       normalize! params
       filter! VALID_REPO_OPTIONS, params
 
-      put_request("/2.0/repositories/#{user}/#{repo.downcase}/", DEFAULT_REPO_OPTIONS.merge(params))
+      put_request("/1.0/repositories/#{user}/#{repo.downcase}/", DEFAULT_REPO_OPTIONS.merge(params))
     end
 
     # Get a repository
@@ -223,7 +223,7 @@ module BitBucket
       _update_user_repo_params(user_name, repo_name)
       _validate_user_repo_params(user, repo) unless user? && repo?
 
-      delete_request("/2.0/repositories/#{user}/#{repo.downcase}")
+      delete_request("/1.0/repositories/#{user}/#{repo.downcase}")
     end
 
     # List repositories for the authenticated user
@@ -274,7 +274,7 @@ module BitBucket
       _validate_user_repo_params(user, repo) unless user? && repo?
       normalize! params
 
-      response = get_request("/2.0/repositories/#{user}/#{repo.downcase}/tags/", params)
+      response = get_request("/1.0/repositories/#{user}/#{repo.downcase}/tags/", params)
       return response unless block_given?
       response.each { |el| yield el }
     end
